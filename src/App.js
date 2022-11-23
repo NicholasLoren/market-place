f6b1b8bf910dc1446460b22df0b21034952b324a
NicholasLoren <lorennicosir@gmail.com>
2022-11-23 15:25:51 +0300

    Created routes for the application

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ForgotPassword from './pages/ForgotPassword'
+import Explore from './pages/Explore'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Offers from './pages/Offers'
+
 function App() {
   return (
     <>
-      <h1>App</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/forgotpassword" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
     </>
   )
 }
