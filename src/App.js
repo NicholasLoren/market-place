b08d6fb01be117181501fb5075c8a2aa3bef256b
NicholasLoren <lorennicosir@gmail.com>
2022-11-28 16:19:54 +0300

    Fixed the routing temporary changes

--- src/App.js
+++ src/App.js
@@ -8,17 +8,13 @@ import SignUp from './pages/SignUp'
 import Offers from './pages/Offers'
 
 function App() {
-  const loggedIn = null
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
-          <Route
-            path="/profile"
-            element={loggedIn === undefined ? <Profile /> : <SignIn />}
-          />
+          <Route path="/profile" element={<Profile />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
