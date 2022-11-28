5c222c2bdd518472ba5dcb587c092c8cae1bd20e
NicholasLoren <lorennicosir@gmail.com>
2022-11-28 16:33:58 +0300

    Added a container for toast notifications

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import NavBar from './components/NavBar'
 import ForgotPassword from './pages/ForgotPassword'
 import Explore from './pages/Explore'
@@ -21,6 +23,7 @@ function App() {
         </Routes>
         <NavBar />
       </Router>
+      <ToastContainer />
     </>
   )
 }
