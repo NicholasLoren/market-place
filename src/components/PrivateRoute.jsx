ed1b24128b3468be9979295964eaad786130c012
NicholasLoren <lorennicosir@gmail.com>
2022-11-30 01:07:32 +0300

    Added loading spinner when checking login

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,10 +1,10 @@
 import { Navigate, Outlet } from 'react-router-dom'
 import { useAuthStatus } from '../hooks/useAuthStatus'
-
+import Spinner from './Spinner'
 const PrivateRoute = () => {
   const { loggedIn, checkStatus } = useAuthStatus()
 
-  if (checkStatus) return <h3>Loading....</h3>
+  if (checkStatus) return <Spinner />
 
   return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />
 }
