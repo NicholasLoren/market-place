dbe66fac61c90bc6d0245b0c79cc83ae6126410f
NicholasLoren <lorennicosir@gmail.com>
2022-12-01 02:54:29 +0300

    Created single list item

---
+++ src/components/ListingItem.jsx
@@ -0,0 +1,61 @@
+import { Link } from 'react-router-dom'
+import { ReactComponent as DeleteIcon } from '../assets/svg/deleteIcon.svg'
+import bedIcon from '../assets/svg/bedIcon.svg'
+import bathtubIcon from '../assets/svg/bathtubIcon.svg'
+
+function ListingItem({ listing, id, onDelete }) {
+  return (
+    <li className="categoryListing">
+      <Link
+        to={`category/${listing.type}/${id}`}
+        className="categoryListingLink"
+      >
+        <img
+          src={listing.imageUrls[0]}
+          alt={listing.name}
+          className="categoryListingImg"
+        />
+
+        <div className="categoryDListingDetails">
+          <p className="categoryListingLocation">{listing.location}</p>
+          <p className="categoryListingName">{listing.name}</p>
+          <p className="categoryListingPrice">
+            $
+            {listing.offer
+              ? listing.discountPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+              : listing.regularPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            {listing.type === 'rent' && '/ Month'}
+          </p>
+          <div className="categoryListingInfoDiv">
+            <img src={bedIcon} alt="bed" />
+            <p className="categoryListingInfoText">
+              {listing.bedrooms > 1
+                ? `${listing.bedrooms} Bedrooms`
+                : `${listing.bedrooms} Bedroom`}
+            </p>
+            <img src={bathtubIcon} alt="bed" />
+            <p className="categoryListingInfoText">
+              {listing.bathrooms > 1
+                ? `${listing.bathrooms} Bathrooms`
+                : `${listing.bathrooms} Bathroom`}
+            </p>
+          </div>
+        </div>
+      </Link>
+
+      {onDelete && (
+        <DeleteIcon
+          fill="#fa7660"
+          className="removeIcon"
+          onClick={() => onDelete()}
+        />
+      )}
+    </li>
+  )
+}
+
+export default ListingItem
